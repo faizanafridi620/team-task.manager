508da0b2bd9c28904ec2b5a18849014d0658c2bd
faizanafridi620 <faizanafridi676@gmail.com>
2026-05-10 22:13:23 +0530

    Fix producion api

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: [
+        "http://localhost:5173",
         "https://team-taskmanager-production-39f8.up.railway.app"
     ],
     credentials: true
